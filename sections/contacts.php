89cd8a2e585e6c04dc5d94b6d5ce98f9c189cbf1
Charles Harvey <harvey.charles@gmail.com>
2017-08-06 23:13:58 +0100

    aded address to contacts section

--- sections/contacts.php
+++ sections/contacts.php
@@ -1,21 +1,33 @@
 <div class="container">
-<ul class="people">
+
+    <h2>Lausanne</h2>
+    <p>
+        Place Pepinet, 1 <br />
+        1003 Lausanne - Switzerland <br />
+        TEL : +41 21 310 31 31 <br />
+        FAX : +41 21 310 31 00 <br />
+        <a href="mailto:reception@ifchor.ch">reception@ifchor.ch</a>
+    </p>
 
 
-    <?php for ($i=0; $i < 5 ; $i++) { ?>
 
-        <li class="person">
+    <ul class="people">
 
-            <div class="profile_picture">
-                <div class="image_from_background" style="background-color: #f1f1f1;"></div>
-            </div>
-            <div class="profile_details">
-                <h2>Person Name <?php echo $i; ?></h2>
-                <p>Phone: 12345 <br />
-                    Email: email@website.com</p>
+
+        <?php for ($i=0; $i < 5 ; $i++) { ?>
+
+            <li class="person">
+
+                <div class="profile_picture">
+                    <div class="image_from_background" style="background-color: #f1f1f1;"></div>
                 </div>
+                <div class="profile_details">
+                    <h2>Person Name <?php echo $i; ?></h2>
+                    <p>Phone: 12345 <br />
+                        Email: email@website.com</p>
+                    </div>
 
-        </li>
-        <?php };   ?>
-    </ul>
-</div>
+                </li>
+                <?php };   ?>
+            </ul>
+        </div>
