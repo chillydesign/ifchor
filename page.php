e1a244cbe9dba5301fe13a11d5ba3f08d4d81ac6
Mélissa Rissel <rissel.melissa@gmail.com>
2017-08-06 14:58:31 +0200

    header video

--- page.php
+++ page.php
@@ -4,6 +4,32 @@
 
 		<!-- article -->
 		<article>
+			<section class="section_photo_slide">
+			<?php $background_image =  get_field('background_image'); ?>
+			<?php $size =  get_field('size'); ?>
+			<?php if (is_array($size) ) $size = $size[0]; ?>
+
+			<div class="photo_slide_container  photo_slide_size_<?php echo $size;  ?>">
+
+					<div class="photo_slide_content">
+						<?php echo get_field('content'); ?>
+					</div>
+
+					<?php if(get_field('video')){ ?>
+						<video id="vidbg" autoplay="autoplay" loop="loop" >
+							<source src="<?php echo get_field('video-mp4')['url']; ?>" />
+							<source src="<?php echo get_field('video-ogv')['url']; ?>" />
+						</video>
+					<?php } ?>
+					<div class="parallax photo_slide " style="background-image: url(<?php echo $background_image['url']; ?>);"></div>
+					<div class="photo_gradient"></div>
+
+			</div>
+		</section>
+
+
+
+
 			<?php include('section-loop.php'); ?>
 		</article>
 		<!-- /article -->
---
+++ sections/locations.php
@@ -0,0 +1,19 @@
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm-8">
+			<?php include 'locations_map.svg'; ?>
+		</div>
+		<div class="col-sm-4">
+			<div class="description">
+				<h2>Description of the map</h2>
+				<p>
+					We can put some text or contact information over here.
+				</p>
+				<p>
+					The information can change dynamically when the user clicks or hovers on a point on the map. For example the name of the office, its specialties, opening times and contact info can be displayed upon hovering over the correct point. Another possible animation would be to have the names of all the offices show on the right. Upon hovering on the name the correct point lights up on the map.
+				</p>
+
+			</div>
+		</div>
+	</div>
+</div>
