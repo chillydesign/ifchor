675e21b92688cd92bff768dd729361f4a519439d
Charles Harvey <harvey.charles@gmail.com>
2021-05-19 10:52:49 +0100

    add zug to locations map

--- sections/locations.php
+++ sections/locations.php
@@ -6,7 +6,7 @@
 
         <div class="location_box_australia_1_ location_box">
             <h3>Australia</h3>
-            <p>Level 23, 360 Collins Street  <br />
+            <p>Level 23, 360 Collins Street <br />
                 Melbourne VIC 3000 <br />
                 +61 (0)410 668 506 <br />
                 <a href="https://www.google.co.uk/maps/place/360+Collins+St,+Melbourne+VIC+3000,+Australia/@-37.8163265,144.9601497,17z/data=!3m1!4b1!4m5!3m4!1s0x6ad642b4a2bcf095:0x71c25fe89fa00c!8m2!3d-37.8163265!4d144.9623384" target="_blank">Google Maps</a>
@@ -81,77 +81,83 @@
             <h3>Switzerland</h3>
 
             <div class="row">
-                <div class="col-sm-6">
+                <div class="col-sm-4">
                     <h4>Lausanne</h4>
                     <p>Place Pepinet, 1 <br />
                         1003 Lausanne <br />
                         +41 21 310 31 31<br />
-                <a target="_blank" href="https://www.google.com/maps/place/Place+P%C3%A9pinet+1,+1003+Lausanne,+Switzerland/@46.5214211,6.6301708,17z/data=!3m1!4b1!4m5!3m4!1s0x478c2e322ff4e049:0x2ee6fe53927a702b!8m2!3d46.5214174!4d6.6323595">Google Maps</a>
+                        <a target="_blank" href="https://www.google.com/maps/place/Place+P%C3%A9pinet+1,+1003+Lausanne,+Switzerland/@46.5214211,6.6301708,17z/data=!3m1!4b1!4m5!3m4!1s0x478c2e322ff4e049:0x2ee6fe53927a702b!8m2!3d46.5214174!4d6.6323595">Google Maps</a>
 
-                    </div>
-                    <div class="col-sm-6">
-                        <h4>Geneva</h4>
-                        <p>Rue de Chantepoulet 5 <br />
-                            1201 Genève <br />
-                            +41 21 310 31 34<br />
+                </div>
+                <div class="col-sm-4">
+                    <h4>Geneva</h4>
+                    <p>Rue de Chantepoulet 5 <br />
+                        1201 Genève <br />
+                        +41 21 310 31 34<br />
                         <a target="_blank" href="https://www.google.com/maps/place/Rue+de+Chantepoulet+5,+1201+Gen%C3%A8ve,+Switzerland/@46.2078834,6.1428574,17z/data=!3m1!4b1!4m5!3m4!1s0x478c65263173e753:0x62935aafda94c86b!8m2!3d46.2078797!4d6.1450461">Google Maps</a>
-
-                        </div>
-                    </div>
-
-
-
-
-                    <a href="#" class="close_location_box">X</a>
                 </div>
-                <div class="location_box_singapore_1_ location_box">
-                    <h3>Singapore</h3>
-                    <p>One George Street #07-04 <br />
-                        Singapore 049145<br />
-                        +65 6908 150<br />
-                    <a target="_blank" href="https://www.google.com/maps/place/7+George+St,+Singapore/@1.2857556,103.8459901,17z/data=!3m1!4b1!4m5!3m4!1s0x31da190b9f46e719:0xbd5bca6bf08103ae!8m2!3d1.2857502!4d103.8481788">Google Maps</a>
-                    </p>
-                    <a href="#" class="close_location_box">X</a>
+                <div class="col-sm-4">
+                    <h4>Zug</h4>
+                    <p>Industriestrasse 24, <br />
+                        6300 Zug <br />
+                        +41 58 411 31 57<br />
+                        <a target="_blank" href="https://www.google.com/maps/place/Industriestrasse+24,+6300+Zug,+Switzerland/@47.1750911,8.5187759,18.2z">Google Maps</a>
                 </div>
-                <div class="location_box_italy_1_ location_box">
-                    <h3>Italy</h3>
-                    <p>
-                      Via G. D'Annunzio 2/101<br />
-                      Genova, 16122<br />
-                      <a target="_blank" href="https://www.google.com/maps/place/Via+Gabriele+d'Annunzio,+2,+16121+Genova+GE,+Italy/@44.4046664,8.9329545,17z/data=!3m1!4b1!4m5!3m4!1s0x12d343dcd68b2821:0x5c678aaf3b14fe4a!8m2!3d44.4046626!4d8.9351432">Google Maps</a>
-
-                    </p>
-                    <a href="#" class="close_location_box">X</a>
-                </div>
-                <div class="location_box_seattle location_box">
-                    <h3>Seattle, USA</h3>
-                    <p>
-                        1221 1st Ave, Room 1211 <br />
-                        Seattle, WA 98101<br />
-                     <a target="_blank" href="https://www.google.com/maps/place/1221+1st+Ave+%231211,+Seattle,+WA+98101,+USA/@47.606347,-122.3403869,17z/data=!3m1!4b1!4m5!3m4!1s0x54906ab18a225d71:0xf850f7beae1a062c!8m2!3d47.6063434!4d-122.3381982">Google Maps</a>
-                    </p>
-
-                    <a href="#" class="close_location_box">X</a>
-                </div>
-                <div class="location_box_dubai location_box">
-                    <h3>Dubai</h3>
-                    <p>
-                      2501 Westburry Tower<br />
-                      Business Bay, Dubai - UAE<br />
-                       <a target="_blank" href="https://www.google.com/maps/search/2501+Westburry+Tower+Business+Bay,+Dubai+-+UAE/@25.1859372,55.2724433,17z/data=!3m1!4b1">Google Maps</a>
-                    </p>
-
-                    <a href="#" class="close_location_box">X</a>
-                </div>
-
-                <div class="location_box_india location_box">
-                    <h3>India</h3>
-                    <p>
-                      Mumbai
-                    </p>
-
-                    <a href="#" class="close_location_box">X</a>
-                </div>
-
             </div>
+
+
+
+
+            <a href="#" class="close_location_box">X</a>
         </div>
+        <div class="location_box_singapore_1_ location_box">
+            <h3>Singapore</h3>
+            <p>One George Street #07-04 <br />
+                Singapore 049145<br />
+                +65 6908 150<br />
+                <a target="_blank" href="https://www.google.com/maps/place/7+George+St,+Singapore/@1.2857556,103.8459901,17z/data=!3m1!4b1!4m5!3m4!1s0x31da190b9f46e719:0xbd5bca6bf08103ae!8m2!3d1.2857502!4d103.8481788">Google Maps</a>
+            </p>
+            <a href="#" class="close_location_box">X</a>
+        </div>
+        <div class="location_box_italy_1_ location_box">
+            <h3>Italy</h3>
+            <p>
+                Via G. D'Annunzio 2/101<br />
+                Genova, 16122<br />
+                <a target="_blank" href="https://www.google.com/maps/place/Via+Gabriele+d'Annunzio,+2,+16121+Genova+GE,+Italy/@44.4046664,8.9329545,17z/data=!3m1!4b1!4m5!3m4!1s0x12d343dcd68b2821:0x5c678aaf3b14fe4a!8m2!3d44.4046626!4d8.9351432">Google Maps</a>
+
+            </p>
+            <a href="#" class="close_location_box">X</a>
+        </div>
+        <div class="location_box_seattle location_box">
+            <h3>Seattle, USA</h3>
+            <p>
+                1221 1st Ave, Room 1211 <br />
+                Seattle, WA 98101<br />
+                <a target="_blank" href="https://www.google.com/maps/place/1221+1st+Ave+%231211,+Seattle,+WA+98101,+USA/@47.606347,-122.3403869,17z/data=!3m1!4b1!4m5!3m4!1s0x54906ab18a225d71:0xf850f7beae1a062c!8m2!3d47.6063434!4d-122.3381982">Google Maps</a>
+            </p>
+
+            <a href="#" class="close_location_box">X</a>
+        </div>
+        <div class="location_box_dubai location_box">
+            <h3>Dubai</h3>
+            <p>
+                2501 Westburry Tower<br />
+                Business Bay, Dubai - UAE<br />
+                <a target="_blank" href="https://www.google.com/maps/search/2501+Westburry+Tower+Business+Bay,+Dubai+-+UAE/@25.1859372,55.2724433,17z/data=!3m1!4b1">Google Maps</a>
+            </p>
+
+            <a href="#" class="close_location_box">X</a>
+        </div>
+
+        <div class="location_box_india location_box">
+            <h3>India</h3>
+            <p>
+                Mumbai
+            </p>
+
+            <a href="#" class="close_location_box">X</a>
+        </div>
+
+    </div>
+</div>
