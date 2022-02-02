7f030df25de45ca78e08281efcdd0d46b4cef2bd
Charles Harvey <harvey.charles@gmail.com>
2022-02-02 21:47:48 +0100

    add turbine svg to service

--- sections/services.php
+++ sections/services.php
@@ -1,49 +1,53 @@
-
 <div class="container">
   <div class="services_container">
 
     <div class="service_box">
       <a href="<?php the_sub_field('link1'); ?>">
         <?php include('drybulk.svg'); ?>
-          <h4><?php the_sub_field('title1'); ?></h4>
+        <h4><?php the_sub_field('title1'); ?></h4>
       </a>
     </div>
 
     <div class="service_box">
       <a href="<?php the_sub_field('link2'); ?>">
         <?php include('tankers.svg'); ?>
-          <h4><?php the_sub_field('title2'); ?></h4>
+        <h4><?php the_sub_field('title2'); ?></h4>
       </a>
     </div>
 
     <div class="service_box">
       <a href="<?php the_sub_field('link3'); ?>">
         <?php include('sale-purchase.svg'); ?>
-          <h4><?php the_sub_field('title3'); ?></h4>
+        <h4><?php the_sub_field('title3'); ?></h4>
       </a>
     </div>
 
     <div class="service_box">
       <a href="<?php the_sub_field('link4'); ?>">
         <?php include('ship-finance.svg'); ?>
-          <h4><?php the_sub_field('title4'); ?></h4>
+        <h4><?php the_sub_field('title4'); ?></h4>
       </a>
     </div>
 
     <div class="service_box">
       <a href="<?php the_sub_field('link5'); ?>">
         <?php include('marine-technology.svg'); ?>
-          <h4><?php the_sub_field('title5'); ?></h4>
+        <h4><?php the_sub_field('title5'); ?></h4>
       </a>
     </div>
 
     <div class="service_box">
       <a href="<?php the_sub_field('link6'); ?>">
         <?php include('research.svg'); ?>
-          <h4><?php the_sub_field('title6'); ?></h4>
+        <h4><?php the_sub_field('title6'); ?></h4>
       </a>
     </div>
-
+    <div class="service_box">
+      <a href="<?php the_sub_field('link7'); ?>">
+        <?php include('turbine.svg'); ?>
+        <h4><?php the_sub_field('title7'); ?></h4>
+      </a>
+    </div>
 
   </div>
 </div><!--  END OF CONTAINER -->
